c45645f8095fa06044b51b433031f82aea572a23
Shreya Suresh <shreya4763@gmail.com>
2023-02-19 11:29:22 -0800

    got basic functions working

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 /*
 Shreya Suresh
-10/24/22
-Student List- Create a list of students. You can add, delete, or print students. You can also quit.
+2/19/23
  */
 #include <iostream>
 #include <vector>
 #include <cstring>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -15,73 +15,71 @@ struct Student {
   char lastName[10];
   int id;
   float gpa;
+  Student* next;
 };
-void addStu(vector<Student*> &Vector);
-void deleteStu(vector<Student*> &Vector);
-void printStu(vector<Student*> &Vector);
+void addStu(Student* htable[], int &curID);
+void dlt(Student* htable[]);
+void print(Student* htable[], int size);
+void printChain(Student* current);
+void reset(Student* htable[], int size);
 
+void addStu(Student* htable[], int &curID){
+  Student* newPoint = new Student();
+  strcpy(newPoint->firstName, "S");
+  strcpy(newPoint->lastName, "R");
+  newPoint->id = curID;
+  newPoint->gpa = 4.12;
+  htable[(curID%100)] = newPoint;
+  curID++;
+  return;
+}
+void print(Student* htable[], int size){
+  for(int i = 0; i < size; i++){
+    printChain(htable[i]);
+  }
+  return;
+}
+void printChain(Student* current){
+  if(current != NULL){
+    cout << current->firstName << " " << current->lastName << endl;
+    printChain(current->next);
+  }
+  return;
+}
+void dlt(Student* htable[]){
+
+}
+void reset(Student* htable[], int size){
+  for(int i = 0; i < size; i++){
+    htable[i] = NULL;
+  }
+}
 int main(){
   bool running = true;
-  vector<Student*> Vector;
+  Student* htable[100];
   char input[10];
-  
-  while(running == true){ // check user input
+  int curID = 0;
+  int size = sizeof(htable)/sizeof(htable[0]);
+  reset(htable, size);
+
+  while(running == true){ // check user input                                                                                                            
   cout << "Enter a command(ADD, DELETE, PRINT, or QUIT)" << endl;
   cin.getline(input, 10);
-  for (int i = 0; i < 10; i++){ // convert input to uppercase
+  for (int i = 0; i < 10; i++){ // convert input to uppercase                                                                                            
     input[i] =  toupper(input[i]);
   }
-  if (strcmp(input, "ADD") == 0){ // run add function- add student to list
-    addStu(Vector);
-  } else if (strcmp(input, "DELETE") == 0){ // run delete function- remove student from list
-    deleteStu(Vector);
-  } else if (strcmp(input, "PRINT") == 0){ // run print function- display student list
-    printStu(Vector);
-  } else if(strcmp(input, "QUIT") == 0) { //quit program
+  if (strcmp(input, "ADD") == 0){
+    addStu(htable, curID);
+  } else if (strcmp(input, "DELETE") == 0){
+
+  } else if (strcmp(input, "PRINT") == 0){
+    cout << sizeof(htable) << endl;
+    print(htable, size);
+  } else if(strcmp(input, "QUIT") == 0) {
     running = false;
   }else {
     cout << "You can only ADD, DELETE, PRINT, or QUIT" << endl;
   }
   }
   return 0;
-}
-void addStu(vector<Student*> &Vector){ // Add a new student to the list
-    Student* student = new Student;
-    cout << "Enter First name: " << endl;
-    cin >> student->firstName;
-    cin.ignore(10, '\n');
-    cout << "Enter Last name: " << endl;
-    cin >> student->lastName;
-    cin.ignore(10, '\n');
-    cout << "Enter ID: " << endl;
-    cin >> student->id;
-    cin.ignore(10, '\n');
-    cout << "Enter GPA: " << endl;
-    cin >> student->gpa;
-    cin.ignore(10, '\n');
-
-    Vector.push_back(student);
-}
-void deleteStu(vector<Student*> &Vector){ // delete a student from the list
-    cout << "Enter a student ID to remove a student from the list." << endl;
-    int num = 0;
-    cin >> num;
-    cin.ignore(10, '\n');
-    for (vector<Student*>::iterator it = Vector.begin(); it != Vector.end(); it++){ //iterate through list
-      if((*it)-> id == num){ // check if input is an existing id 
-	delete Vector[it-Vector.begin()];
-	Vector.erase(it); // erase student from list
-	break;
-      } else { 
-	cout << "Invalid Student ID." << endl;
-      }
-    }
-}
-void printStu(vector<Student*> &Vector){ // display student list
-    for(vector<Student*>::iterator it = Vector.begin(); it != Vector.end(); it++){
-      cout << (*it) -> firstName << " ";
-      cout << (*it) -> lastName << ", ";
-      cout << (*it) ->  id << ",";
-      cout << fixed << setprecision(2) << (*it) -> gpa << endl; // round gpa to 2 places
-    }
 }
