d69ea88873e25caea17c86543725005b3d6d0851
Shreya Suresh <shreya4763@gmail.com>
2023-02-19 15:46:52 -0800

    added comments, done now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*A
 Shreya Suresh
 2/19/23
+Hash Table- Create a hash table and a random student generator
  */
 #include <iostream>
 #include <vector>
@@ -17,7 +18,7 @@ struct Student {
   float gpa;
   Student* next;
  };
-
+//function prototypes
 void dlt(Student** htable, int size);
 void printAll(Student** htable, int size);
 void printChain(Student* current);
@@ -27,7 +28,7 @@ Student** mAdd(Student** htable, int &curID, int &size);
 Student** rehash(Student** htable, Student** ntable, int &size, int curID);
 
 
-Student** addStu(Student** htable, int &curID, int &size) {  
+Student** addStu(Student** htable, int &curID, int &size) { //add students using random student generator  
   int students;
   cout << "How many students are you adding?" << endl;
   cin >> students; 
@@ -42,6 +43,7 @@ Student** addStu(Student** htable, int &curID, int &size) {
     int num2 = (rand() % 20) + 1;
     Student* newPoint = new Student();
     count = 1;
+    //retrieve first name from file
     while (ffile.getline(input,100, '\n')) {
       if (count == num) {
 	strcpy(firstName,input);
@@ -51,6 +53,7 @@ Student** addStu(Student** htable, int &curID, int &size) {
   }
   ffile.close();
   count = 1;
+  //retrieve last name from file
   while (lfile.getline(input,100, '\n')) {
     if (count == num2) {
       strcpy(lastName,input);
@@ -59,6 +62,7 @@ Student** addStu(Student** htable, int &curID, int &size) {
     count++;
   }
   lfile.close();
+  //random GPA generator
   float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
   r *= 23;
   while(r > 4){
@@ -71,19 +75,19 @@ Student** addStu(Student** htable, int &curID, int &size) {
   strcpy(newPoint->lastName, lastName);
   newPoint->id = curID;
   newPoint->gpa = r;
-  if(htable[(curID)%size] == NULL){
+  if(htable[(curID)%size] == NULL){ //case of no collisions
     htable[(curID%size)] = newPoint;
   }
   else{
-    if(htable[(curID)%size]->next == NULL){
+    if(htable[(curID)%size]->next == NULL){ //case of one collision
       htable[curID%size]->next = newPoint;
     }
     else{
-      if(htable[(curID)%size]->next->next == NULL){
+      if(htable[(curID)%size]->next->next == NULL){ //case of two collisions
 	htable[curID%size]->next->next = newPoint;
       }
-      else{
-	Student** new_table = new Student*[size*2];
+      else{ //case of three collisions, prompt to create a new table
+	Student** new_table = new Student*[size*2]; //making a new table
 	reset(new_table, size*2);
 	htable = rehash(htable, new_table, size, curID);
 	htable[curID%size]->next = newPoint;
@@ -95,7 +99,7 @@ Student** addStu(Student** htable, int &curID, int &size) {
 return htable;
   
 }
-Student** mAdd(Student** htable, int &curID, int &size){
+Student** mAdd(Student** htable, int &curID, int &size){ //add students manually
   char input[100];
   char firstName[100];
   char lastName[100];
@@ -135,17 +139,17 @@ Student** mAdd(Student** htable, int &curID, int &size){
    curID++;
    return htable;
 }
-Student** rehash(Student** htable, Student** ntable, int &size, int curID){
+Student** rehash(Student** htable, Student** ntable, int &size, int curID){ //rehash students into new table
   int size1 = size;
   size = size*2;
   for(int i = 0; i < curID; i++){ 
-    if(i < size1){
+    if(i < size1){ //if first node
       ntable[(i%size)] = htable[(i)%size1];
     }
-    else if(i < size1*2){
+    else if(i < size1*2){ //if second node
       ntable[(i%size)] = htable[(i)%size1]->next;
     }
-    else{
+    else{ //if last node
       ntable[i%size]->next = htable[(i)%size1]->next->next;
     }
   }
@@ -155,24 +159,25 @@ Student** rehash(Student** htable, Student** ntable, int &size, int curID){
   return ntable;
 }
 
-void printAll(Student** htable, int size){
+void printAll(Student** htable, int size){ //print out all the students{
   for (int i = 0; i < size; i++){
     printChain(htable[i]);
   }
   return;
 }
-void printChain(Student* current){
+void printChain(Student* current){ //recursively print a chain, makes printing all easier
   if(current != NULL){
     cout << current->firstName << " " << current->lastName << " GPA: " << fixed << setprecision(2) << current->gpa << " " << "ID: " << current->id << endl;
     printChain(current->next);
   }
   return;
 }
-void dlt(Student** htable, int size){
+void dlt(Student** htable, int size){ //delete a specific student
   int i;
   cout << "What is the student's ID?" << endl;
   cin >> i;
   cin.clear();
+  //find that student and override it
   if (htable[i%size]->id == i){
     htable[i%size] = htable[i%size]->next;
   } else if (htable[i%size]->next->id == i){
@@ -183,12 +188,12 @@ void dlt(Student** htable, int size){
   return;
 }
 
-void reset(Student** htable, int size){
+void reset(Student** htable, int size){ //resets the values in the table
   for(int i = 0; i < size; i++){
     htable[i] = NULL;
   }
 }
-int main(){
+int main(){ //main function
   srand(time(0));
   bool running = true;
   Student** htable = new Student*[100];
@@ -202,16 +207,16 @@ int main(){
   for (int i = 0; i < 10; i++){ // convert input to uppercase                                                                                   
     input[i] =  toupper(input[i]);
   }
-  if (strcmp(input, "ADD") == 0){
+  if (strcmp(input, "ADD") == 0){ //call add function
     htable = addStu(htable, curID, size);
-  } else if (strcmp(input, "MADD") == 0){
+  } else if (strcmp(input, "MADD") == 0){ //call manual add function
     htable = mAdd(htable, curID, size);
-    }else if (strcmp(input, "DELETE") == 0){
+  }else if (strcmp(input, "DELETE") == 0){ //call delete function
     dlt(htable, size);
     cout << "Deleted." << endl;
-  } else if (strcmp(input, "PRINT") == 0){
+  } else if (strcmp(input, "PRINT") == 0){ //call print function
     printAll(htable, size);
-  } else if(strcmp(input, "QUIT") == 0) {
+  } else if(strcmp(input, "QUIT") == 0) { //quit progran
     running = false;
   }else {
    
